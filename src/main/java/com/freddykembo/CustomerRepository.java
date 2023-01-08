1e9edd1995a84cb76e3c9d0757758ae99ec6fda5
freddykembo <freddykembo@gmail.com>
2023-01-09 01:30:50 +0300

    added docker yaml file. Updated application properties

--- src/main/java/com/freddykembo/CustomerRepository.java
+++ src/main/java/com/freddykembo/CustomerRepository.java
@@ -1,2 +1,7 @@
-package com.freddykembo;public interface CustomerRepository {
+package com.freddykembo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
 }
--- src/main/java/com/freddykembo/Main.java
+++ src/main/java/com/freddykembo/Main.java
@@ -2,36 +2,82 @@ package com.freddykembo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        GreetResponse response =  new GreetResponse(
-                "Sasa",
-                List.of("Java", "Golang", "PHP"),
-                new Person("Freddy", 30, 35_000));
-
-        return response;
+    @GetMapping
+    public List<Customer> getCustomer() {
+        return customerRepository.findAll();
     }
 
-    record Person(String name, int age, double savings){
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
 
     }
 
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguage,
-            Person person){
-
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
     }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+
+        customerRepository.save(customer);
+    }
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet() {
+//        GreetResponse response =  new GreetResponse(
+//                "Sasa",
+//                List.of("Java", "Golang", "PHP"),
+//                new Person("Freddy", 30, 35_000));
+//
+//        return response;
+//    }
+
+//    record Person(String name, int age, double savings){
+//
+//    }
+//
+//    record GreetResponse(
+//            String greet,
+//            List<String> favProgrammingLanguage,
+//            Person person){
+//
+//    }
 }
